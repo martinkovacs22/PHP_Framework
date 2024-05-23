c3b70a76df3ba2cc98039ea3933dea09270234ab
Kovács Martin <martinkovacs22@gmail.com>
2024-05-23 12:31:50 +0200

    space

--- system/model/TestModel.php
+++ system/model/TestModel.php
@@ -8,7 +8,7 @@ use model\Db;
 use config\Req;
 class TestModel{
         public static function CallProcedure($array,$storeProcedure){
-            Req::CONFIG_OPTIMALIZATION();
+            Req::CONFIG_OPTIMALIZATION(); 
             $db = Db::connectToDatabase($_SESSION["db"]["db_name"], $_SESSION["db"]["db_username"], $_SESSION["db"]["db_pass"],$_SESSION["db"]["db_host"]);
         return Db::Call($storeProcedure, $array, $db);
     }
